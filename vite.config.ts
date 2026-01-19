890ac7c8f4ded084e8300add1500183c0f8afe08
“TRUONG <ly.truong.dn@gmail.com  git config --global user.name Truong Ly>
2026-01-19 12:43:27 +0700

    feat: US-004 - Add Comlink dependencies and Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
+import comlink from "vite-plugin-comlink";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid(), comlink()],
   server: {
     port: 5173,
     strictPort: true,
   },
   build: {
     target: "esnext",
+  },
+  worker: {
+    plugins: () => [comlink()],
   },
 });
