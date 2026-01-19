b826273271156cf58f697bbab3c47794b6f9f2c9
“TRUONG <ly.truong.dn@gmail.com  git config --global user.name Truong Ly>
2026-01-19 13:02:29 +0700

    fix: Remove incomplete Rust command files (cleanup)

--- src-tauri/src/commands/mod.rs
+++ src-tauri/src/commands/mod.rs
@@ -1,9 +1,6 @@
-pub mod command;
 pub mod config;
 pub mod engine;
 pub mod misc;
 pub mod opkg;
 pub mod updater;
 pub mod workspace;
-
-pub use command::{Command, CommandProcessor, CommandResponse, CommandResult, PermissionData, SessionData};
